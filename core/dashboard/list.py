a5e807af894d376464a046eeae0852ea215b2f7e
xudikk <71259765+xudikk@users.noreply.github.com>
2023-09-04 19:03:05 +0500

    second step ready

---
+++ core/dashboard/list.py
@@ -0,0 +1,10 @@
+from django.shortcuts import redirect, render
+
+from base.custom import permission_checker
+from core.models.auth import User
+
+
+@permission_checker
+def list_user(request):
+    users = User.objects.all()
+    return render(request, 'pages/list.html', {'roots': users, 'u_active': "active"})
