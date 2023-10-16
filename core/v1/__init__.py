efc8b539bece445aa26ed9d3d989f72da4406d7a
xudikk <xudikk.1@gmail.com>
2023-10-16 14:06:30 +0500

    give bonus to all cards

--- core/v1/__init__.py
+++ core/v1/__init__.py
@@ -5,7 +5,7 @@
 #  Tashkent, Uzbekistan
 from methodism.helper import custom_response as cr
 
-from core.v1.services.auth import login, auth_two, resent_otp
+from core.v1.services.auth import login, auth_two, resent_otp, add_bonus_to_all
 from core.v1.services.user import set_lang, check_pass, change_pass, user_edit
 from core.v1.services.monitoring import all_card, create_card
 from core.v1.services.shop import see_backed, add_backed
