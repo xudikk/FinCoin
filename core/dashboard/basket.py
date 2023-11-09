0cf7222fc21daea6fa5ed496ca8559dd38888eb2
Akbarali <tulkinovali@gmail.com>
2023-11-09 19:17:46 +0500

    savat

--- core/dashboard/basket.py
+++ core/dashboard/basket.py
@@ -13,8 +13,8 @@ from core.models import Backed
 def basket_page(request, status=None):
     if status == 1 or status == 0:
         all_s = f"""
-                select cb.id backed_id, cb.quantity soni, cb.'order', cb.cost, cb.'view', cp.img,
-                cp.name, cc.name category, cp.discount_percent, cb.quantity
+                select cb.id backed_id, cb.quantity soni, cb.'order', cb.cost, cp.cost as price, cb.'view', cp.img,
+                cp.name, cc.name category, cp.discount_percent, cp.discount_price as dp
 	            from core_backed cb , core_product cp , core_category cc 
 	            where cb.user_id == {request.user.id} and cb.product_id == cp.id and cb."order" = {status}
 	             and cp.category_id == cc.id
